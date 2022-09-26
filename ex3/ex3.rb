394793d3c6eb6e705296406fddd34d615c8aa608
pill <phongdh228@gmail.com>
2022-09-26 15:24:38 +0700

    comments the code

--- ex3/ex3.rb
+++ ex3/ex3.rb
@@ -1,35 +1,42 @@
 def handle_input()
     print 'Insert number of elements (<99):'
-    user_input= gets.chomp.to_i
-
+    user_input = gets.chomp.to_i
+    #check if user input in range of 1 to 99
     while user_input <= 0 || user_input > 99 do
-        print "Please enter a valid number (<99):" 
+        print "Please enter a valid number:" 
         user_input= gets.chomp.to_i
     end
+
+    #create random integer array
     array_created = Array.new(user_input) {rand(1..9)}
     return array_created
 end
 
 def handle_sub_array(array_created)
-    sub_array = []
-    i = 0
-    sub_array_counter = 1
+    sub_array = [] #to store increse arrays
+    i = 0 #run iteration over random array
+    sub_array_counter = 1 #counting number of arrays
     
     array_created.each do |element|
+        #if sub array is empty || previous element of random array is smaller than current element => store current element to sub array
         if sub_array.length == 0 || array_created[i-1] < array_created[i]
-            sub_array << element
+            sub_array << element 
         else
+        #if not, print the sub array and assign it to hold the current element
             puts "Chuoi #{sub_array_counter}: #{sub_array}"
             sub_array = [array_created[i]]
             sub_array_counter += 1
         end
-        i += 1
+        i += 1 #increse iteration
     end
-    puts "Chuoi #{sub_array_counter}: #{sub_array}"
+    puts "Chuoi #{sub_array_counter}: #{sub_array}" #print the last sub array
 end
 
+#=================================================================
+#============== EXECUTE REQUIREMENT OF EXCERCISE 03 ==============
+#=================================================================
 
 array_created = handle_input()
+system "clear"
 p array_created
 handle_sub_array(array_created)
-    

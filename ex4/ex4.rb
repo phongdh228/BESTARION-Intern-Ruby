ca2b83c72ae4703bac2b570ff3504f497ba00990
pill <phongdh228@gmail.com>
2022-10-04 15:49:18 +0700

    clean the code

--- ex4/ex4.rb
+++ ex4/ex4.rb
@@ -1,11 +1,9 @@
 $master_array = Array.new
 $master_hash = Hash.new
 $sum = gets.chomp.to_i
+
+#case
 $master_array = [3, 5, -4, 8, 11, 1, -1, 6]
-
-# def handle_input()
-
-# end
 
 def hash_array(array)
     i =0
@@ -15,22 +13,30 @@ def hash_array(array)
     end
 end
 
-def find_pair(array)
+def find_pair(array = [], sum)
+
+    hash_array(array)
+
     i = 0
     exit_code = false
     result = Array.new
+
     array.each do |value|
         break if exit_code == true
-        if $master_hash[$sum-value].nil? ==false && $master_hash[$sum-value] != i
+
+        #hash[key] has value && difference to i 
+        if $master_hash[sum-value].nil? ==false && $master_hash[sum-value] != i
+
             result << array[i] 
-            result << array[ $master_hash[$sum-value] ]
+            result << array[$master_hash[sum-value]]
+
             exit_code = true
         elsif
             i+=1
         end
     end
+
     p result
 end
 
-hash_array($master_array)
-find_pair($master_array)
+find_pair($master_array, $sum)

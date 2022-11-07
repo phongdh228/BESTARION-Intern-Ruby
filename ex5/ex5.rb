d0374a4bb04c7b6e7e1a2b422de8281350fe930b
Dang Hoang Phong <phongdh228@gmail.com>
2022-11-07 08:58:48 +0700

    change the way approches ex5

--- ex5/ex5.rb
+++ ex5/ex5.rb
@@ -1,22 +1,24 @@
-b = Array.new
-a = Array.new
-
 a = [5,1,22,26,6,-1,8,10]
-b = [1,6,-1,10, 8]
+b = [1,6,-1,10,8]
 
-exit_code = false
-temp_arr = a
-b.each_with_index do |bi, bindex|
-    break if exit_code == true
-    a.each_with_index do |ai, aindex|
-        a.drop(aindex+1) if bi == ai
-    end
-    if bindex == b.length && a.length == 0 
-        exit_code = true
-        puts "Not a duplicated array"
-    else 
-        puts "It's a duplicated array"
-    end
+# exit_code = false
+# temp_arr = a
+# b.each_with_index do |bi, bindex|
+#     break if exit_code == true
+#     a.each_with_index do |ai, aindex|
+#         a.drop(aindex+1) if bi == ai
+#     end
+#     if bindex == b.length && a.length == 0 
+#         exit_code = true
+#         puts "Not a duplicated array"
+#     else 
+#         puts "It's a duplicated array"
+#     end
+# end
+def check(first_array, second_array)
+  if (second_array & first_array) == second_array
+    #execute
+  else 
+    return false
+  end
 end
-
-

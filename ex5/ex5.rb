7a6cc298cb9bbb3106b3a47b291e3b2f14feb26e
Dang Hoang Phong <phongdh228@gmail.com>
2022-11-12 11:12:28 +0700

    exercise 5

--- ex5/ex5.rb
+++ ex5/ex5.rb
@@ -1,24 +1,23 @@
-a = [5,1,22,26,6,-1,8,10]
-b = [1,6,-1,10,8]
 
-# exit_code = false
-# temp_arr = a
-# b.each_with_index do |bi, bindex|
-#     break if exit_code == true
-#     a.each_with_index do |ai, aindex|
-#         a.drop(aindex+1) if bi == ai
-#     end
-#     if bindex == b.length && a.length == 0 
-#         exit_code = true
-#         puts "Not a duplicated array"
-#     else 
-#         puts "It's a duplicated array"
-#     end
-# end
-def check(first_array, second_array)
-  if (second_array & first_array) == second_array
-    #execute
-  else 
-    return false
-  end
+a = [5,1,22,26,6,-1,8,10]
+b = [1,22,26,10]
+
+def is_sub_array?(main_array,sub_array)
+    hash = Hash.new
+    temp_array = Array.new
+    for i in 0...main_array.length
+        hash[sub_array[i]] = true if i < sub_array.length
+        if hash[main_array[i]]
+            temp_array.push(main_array[i])
+            hash[main_array[i]] = false
+       end
+    end 
+
+    if hash.has_value?(true) #if in hash still having true value
+        false
+    else 
+        temp_array == sub_array #check the equality between 2 arrays
+    end
 end
+
+puts is_sub_array?(a,b)
